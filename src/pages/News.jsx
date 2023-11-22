1a148373f9f738a8f8fdc066e155c11d5cd42fe7
shreyyg <68358840+rosered123@users.noreply.github.com>
2023-11-22 07:03:20 +0000

    news page

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -6,7 +6,56 @@ export default function News(){
         <div>
             <Navbar />
             <div>
-                <h1>Access Many Articles and Exciting Reads!</h1>
+                <h1 id="news-heading">News</h1>
+                <div id="news-column">
+    
+                    <div>
+                        <div class="news-section">
+                            <div class="news-image">Enter image</div>
+                            <div>
+                                <h3>Article #1</h3>
+                                <p>Article description</p>
+                                <button class="news-button">Read More</button>
+                            </div>
+                        </div>
+
+                        <div class="news-section">
+                            <div class="news-image">Enter image</div>
+                            <div>
+                                <h3>Article #2</h3>
+                                <p>Article description</p>
+                                <button class="news-button">Read More</button>
+                            </div>
+                        </div>
+
+                        <div class="news-section">
+                            <div class="news-image">Enter image</div>
+                            <div>
+                                <h3>Article #3</h3>
+                                <p>Article description</p>
+                                <button class="news-button">Read More</button>
+                            </div>
+                        </div>
+
+                        <div class="news-section">
+                            <div class="news-image">Enter image</div>
+                            <div>
+                                <h3>Article #4</h3>
+                                <p>Article description</p>
+                                <button class="news-button">Read More</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="list-column">
+                        <h1 id="list-heading">Exciting Reads</h1>
+                        <ul>
+                            <li>Number 1</li>
+                            <li>Number 2</li>
+                            <li>Number 3</li>
+                        </ul>
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
