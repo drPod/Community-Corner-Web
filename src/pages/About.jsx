3dec8fae5e3f1c123b7ea028b1a983b21ba57224
shreyyg <68358840+rosered123@users.noreply.github.com>
2023-11-22 06:49:09 +0000

    changed about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,9 +6,53 @@ export default function About(){
         <div>
             <Navbar />
             <div>
-                <h1>About Us</h1>
-                <h3>What is Community Corner?</h3>
-                <p>Enter stuff about who we are, our purpose, etc</p>
+                <h1 id="about-heading">About Us</h1>
+                <center>
+                    <div id="about-us-navbar">
+                        <a href="">About Us</a>
+                        <a href="">Masthead</a>
+                        <a href="">Advisory Board</a>
+                        <a href="">Join Our Team</a>
+                        <a href="">Write For Us</a>
+                        <a href="">Advertise</a>
+                        <a href="">Diversity and Inclusion</a>
+                        <a href="">Contact Us</a>
+                    </div>
+                </center>
+                
+                <div class="about-section">
+                    <div>
+                        <h3 class="sub-heading">What is Community Corner?</h3>
+                        <p>Enter stuff about who we are, our purpose, etc</p>
+                    </div>
+                    <img src="../assets/Logo.png" alt="Image"/>
+                </div>
+
+                <div class="about-section">
+                    <div>
+                        <h3 class="sub-heading">Who Are We?</h3>
+                        <p>Enter stuff about who we are, our purpose, etc</p>
+                    </div>
+                    <img src="../assets/Logo.png" alt="Image"/>
+                </div>
+
+                <div class="about-section">
+                    <div>
+                        <h3 class="sub-heading">What Do We Publish?</h3>
+                        <p>Enter stuff about who we are, our purpose, etc</p>
+                    </div>
+                    <img src="../assets/Logo.png" alt="Image"/>
+                </div>
+
+                <div class="about-section">
+                    <div>
+                        <h3 class="sub-heading">History of Community Corner</h3>
+                        <p>Enter stuff about who we are, our purpose, etc</p>
+                    </div>
+                    <img src="../assets/Logo.png" alt="Image"/>
+                </div>
+                
+                
             </div>
         </div>
     )
