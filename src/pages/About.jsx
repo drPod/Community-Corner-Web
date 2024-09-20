a2e5584a1c19985f2e606fda42b87aa469706b63
kunioshah <kunioshah@gmail.com>
2024-09-20 14:47:35 -0700

    small push - about and podcast

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,5 @@
+import logo from "../assets/logo.png";
+
 export default function About() {
   return (
     <div>
@@ -9,7 +11,7 @@ export default function About() {
             <h3 className="sub-heading">What is Community Corner?</h3>
             <p>Enter stuff about who we are, our purpose, etc</p>
           </div>
-          <img src="../assets/Logo.png" alt="Image" />
+          <img src={logo} alt="Image" width="200"  />
         </div>
 
         <div className="about-section">
@@ -17,7 +19,7 @@ export default function About() {
             <h3 className="sub-heading">Who Are We?</h3>
             <p>Enter stuff about who we are, our purpose, etc</p>
           </div>
-          <img src="../assets/Logo.png" alt="Image" />
+          <img src={logo} alt="Image" width="200" />
         </div>
 
         <div className="about-section">
@@ -25,15 +27,16 @@ export default function About() {
             <h3 className="sub-heading">What Do We Publish?</h3>
             <p>Enter stuff about who we are, our purpose, etc</p>
           </div>
-          <img src="../assets/Logo.png" alt="Image" />
+          <img src={logo} alt="Image" width="200"  />
         </div>
 
         <div className="about-section">
           <div>
             <h3 className="sub-heading">History of Community Corner</h3>
             <p>Enter stuff about who we are, our purpose, etc</p>
+            <br/>
           </div>
-          <img src="../assets/Logo.png" alt="Image" />
+          <img src={logo} alt="Image" width="200" />
         </div>
       </div>
     </div>
