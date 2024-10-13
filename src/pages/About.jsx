dfbdbb96b893438b57a12b29220ce5eefd7fbbdb
kunioshah <kunioshah@gmail.com>
2024-10-12 20:23:07 -0700

    actual about changes somehow

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,34 +6,91 @@ export default function About() {
 
         <div className="about-section">
           <div>
-            <h3 className="sub-heading">What is Community Corner?</h3>
-            <p>Enter stuff about who we are, our purpose, etc</p>
+            <h3 className="sub-heading">Who We Are</h3>
+            <p className="ml-10 mr-5 text-left">Welcome to the Community Chronicle! The Community Chronicle is the 
+              United States' 1st interscholastic economic cartography newspaper, with a 
+              mission to bridge the economic gap for female veterans post-service and 
+              low-income women. Our platform has grown into an interactive newspaper and
+               podcast with a staff of 17 people from 50 nationalities across 35 cities. </p>
+               <br/>
+            <p className="ml-10 mr-5 text-left">
+            We’ve produced over 2,000 articles and podcasts so far, gained 10,000 Instagram 
+            followers, and garnered 25,000 monthly reads. Our work centers on leveraging knowledge
+             of economic cartography and gender equity to advocate for financial equity for women,
+              especially those who have served in the military.
+            </p>
           </div>
-          <img src="../assets/Logo.png" alt="Image" />
         </div>
 
         <div className="about-section">
           <div>
-            <h3 className="sub-heading">Who Are We?</h3>
-            <p>Enter stuff about who we are, our purpose, etc</p>
+            <h3 className="sub-heading">What We Publish</h3>
+            <p className="ml-10 mr-5 text-left">The Community Chronicle publishes articles and 
+              podcasts focusing on gender equity issues, income inequality, and economic challenges
+               faced by female veterans post-service. We also highlight success stories of veterans
+                who have overcome financial obstacles and excelled in their post-service lives. We offer
+                educational information on how to procure microloans and financial advice.
+                Additionally, we explore economic cartography and how it intersects with broader
+                 humanities topics, analyzing the historical and social impacts of financial systems.
+            </p>
+            <br/>
+            <p className="ml-10 mr-5 text-left">The Community Chronicle also hosts an interactive 
+              podcast series, webinars, and live discussion panels featuring financial experts 
+              and business leaders. Our staff is mentored to be molded into formidable social-activist journalists.
+            </p>
           </div>
-          <img src="../assets/Logo.png" alt="Image" />
         </div>
 
         <div className="about-section">
           <div>
-            <h3 className="sub-heading">What Do We Publish?</h3>
-            <p>Enter stuff about who we are, our purpose, etc</p>
+            <h3 className="sub-heading">History of the Community Chronicle</h3>
+            <p className="ml-10 mr-5 text-left">The Community Chronicle began in 2021 by our
+               founder, Nikhil Kotta, as a collection of favorite economic cartography maps
+               illustrating socioeconomic nequalities and has since evolved into a comprehensive 
+               platform focused on helping female veterans and low-income women navigate post-service 
+               economic challenges.
+            </p>
+            <br/>
+            <p className="ml-10 mr-5 text-left">Through our efforts, we have grown a strong, 
+              active readership and community committed to financial literacy and equity.
+            </p>
+
           </div>
-          <img src="../assets/Logo.png" alt="Image" />
         </div>
 
         <div className="about-section">
           <div>
-            <h3 className="sub-heading">History of Community Corner</h3>
-            <p>Enter stuff about who we are, our purpose, etc</p>
+            <h3 className="sub-heading">General Information</h3>
+            <h3 className="sub-sub-heading"> Submissions, Tips, Op-eds </h3>
+              <p className="ml-10 mr-5 text-left">If you have tips, story ideas, or thoughts on improving financial literacy 
+                and equity, we’d love to hear from you! Email us at communitycornerpodcast@gmail.com 
+                or message us on Instagram at @community_chronicle.
+             </p>
+              <br/>
+
+              <p className="ml-10 mr-5 text-left">We welcome op-eds and article submissions. Articles 
+               can range from 450–1,500 words and will be reviewed prior to publication. Please send all 
+               submissions to communitycornerpodcast@gmail.com.
+              </p>
+              <br/>
+
+            <h3 className="sub-sub-heading"> Comment Policy </h3>
+              <p className="ml-10 mr-5 text-left">We believe in promoting free, open discussions. However, any 
+                comments that contain offensive language, false claims, or disparage people based on race, gender,
+                 religion, or sexual orientation will be removed.
+             </p>
+             <br/>
+              
+             <h3 className="sub-sub-heading"> Advertise With Us </h3>
+              <p className="ml-10 mr-5 text-left">The Community Chronicle offers advertising opportunities 
+                to organizations and businesses committed to advancing financial literacy and equity for female
+                 veterans and low-income women. For more information about advertising, email us at 
+                 communitycornerpodcast@gmail.com.
+
+             </p>
+             <br/>
+
           </div>
-          <img src="../assets/Logo.png" alt="Image" />
         </div>
       </div>
     </div>
